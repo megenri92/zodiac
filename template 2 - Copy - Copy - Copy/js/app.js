93bd8ec0ad6097d9ff6f19ddc636d21d0b705414
Megan Enriquez <meganvue10@gmail.com>
2017-09-07 20:29:46 -0700

    First Commit

---
+++ template 2 - Copy - Copy - Copy/js/app.js
@@ -0,0 +1,63 @@
+var horoscopes = [{
+	name: "Aries",
+	img: "img/aries.png",
+	detail: "March 21 - April 19 <br><u>Strengths</u>: Courageous, determined, confident, enthusiastic, optimistic, honest, and passionate. <br><u>Weaknesses</u>:Impatient, moody, short-tempered, impulsive, and aggressive. <br><u>Description</u>: As the first sign in the zodiac, the presence of Aries always marks the beginning of something energetic and turbulent. They are continuously looking for dynamic, speed and competition, always being the first in everything from work to social gatherings. Thanks to its ruling planet Mars and the fact it belongs to the element of Fire, Aries is one of the most active zodiac signs. It is in their nature to take action, sometimes before they think about it well."
+}, {
+	name: "Taurus",
+	img: "img/taurus.png",
+	detail: "April 20 - May 20 <br><u>Strengths</u>: Reliable, patient, practical, devoted, responsible, and stable. <br><u>Weaknesses</u>: Stubborn, possessive, and uncompromising. <br><u>Description</u>: Practical and well-grounded, Taurus is the sign that harvests the fruits of labor. They feel the need to always be surrounded by love and beauty, turned to the material world, hedonism, and physical pleasures. People born with their Sun in Taurus are sensual and tactile, considering touch and taste the most important of all senses. Stable and conservative, this is one of the most reliable signs of the zodiac, ready to endure and stick to their choices until they reach the point of personal satisfaction."
+}, {
+	name: "Gemini",
+	img: "img/gemini.png",
+	detail: "May 21 - June 20 <br><u>Strengths</u>: Gentle, affectionate, curious, adaptable, ability to learn quickly and exchange ideas. <br><u>Weaknesses</u>: Nervous, inconsistent, and indecisive. <br><u>Description</u>:Expressive and quick-witted, Gemini represents two different personalities in one and you will never be sure which one you will face. They are sociable, communicative and ready for fun, with a tendency to suddenly get serious, thoughtful and restless. They are fascinated with the world itself, extremely curious, with a constant feeling that there is not enough time to experience everything they want to see."
+}, {
+	name: "Cancer",
+	img: "img/cancer.png",
+	detail: "June 21 - July 22 <br><u>Strengths</u>: Tenacious, highly imaginative, loyal, emotional, sympathetic, and persuasive. <br><u>Weaknesses</u>: Moody, pessimistic, suspicious, manipulative, and insecure. <br><u>Description</u>: Deeply intuitive and sentimental, Cancer can be one of the most challenging zodiac signs to get to know. They are very emotional and sensitive, and care deeply about matters of the family and their home. Cancer is sympathetic and attached to people they keep close. Those born with their Sun in Cancer are very loyal and able to empathize with other people's pain and suffering."
+}, {	
+	name: "Leo",
+	img: "img/leo.png",
+	detail: "July 23 - August 22 <br><u>Strengths</u>: Creative, passionate, generous, warm-hearted, cheerful, and humorous. <br><u>Weaknesses</u>:Arrogant, stubborn, self-centered, lazy, and inflexible. <br><u>Description</u>: People born under the sign of Leo are natural born leaders. They are dramatic, creative, self-confident, dominant and extremely difficult to resist, able to achieve anything they want to in any area of life they commit to. There is a specific strength to a Leo and their king of the jungle status. Leo often has many friends for they are generous and loyal. Self-confident and attractive, this is a Sun sign capable of uniting different groups of people and leading them as one towards a shared cause, and their healthy sense of humor makes collaboration with other people even easier."
+}, {
+	name:"Virgo",
+	img: "img/virgo.png",
+	detail: "August 23 - September 22 <br><u>Strengths</u>: Loyal, analytical, kind, hardworking, and practical. <br><u>Weaknesses</u>: Shyness, worry, overly critical of self and others, all work and no play. <br><u>Description</u>:Virgos are always paying attention to the smallest details and their deep sense of humanity makes them one of the most careful signs of the zodiac. Their methodical approach to life ensures that nothing is left to chance, and although they are often tender, their heart might be closed for the outer world. This is a sign often misunderstood, not because they lack the ability to express, but because they won’t accept their feelings as valid, true, or even relevant when opposed to reason. The symbolism behind the name speaks well of their nature, born with a feeling they are experiencing everything for the first time."
+}, {
+	name: "Libra",
+	img: "img/libra.png",
+	detail: "September 23 - October 22 <br><u>Strengths</u>: Cooperative,diplomatic, gracious, fair-minded, and social. <br><u>Weaknesses</u>: Indecisive, avoids confrontations, will carry a grudge, and self-pity. <br><u>Description</u>: People born under the sign of Libra are peaceful, fair, and they hate being alone. Partnership is very important for them, as their mirror and someone giving them the ability to be the mirror themselves. These individuals are fascinated by balance and symmetry, they are in a constant chase for justice and equality, realizing through life that the only thing that should be truly important to themselves in their own inner core of personality. This is someone ready to do nearly anything to avoid conflict, keeping the peace whenever possible."
+}, {	
+	name: "Scorpio",
+	img: "img/scorpio.png",
+	detail: "October 23 - November 21 <br><u>Strengths</u>: Resourceful, brave, passionate, stubborn, and a true friend. <br><u>Weaknesses</u>: Distrusting, jealous, secretive, and violent. <br><u>Description</u>: Scorpio-born are passionate and assertive people. They are determined and decisive, and will research until they find out the truth. Scorpio is a great leader, always aware of the situation and also features prominently in resourcefulness. Scorpio is a Water sign and lives to experience and express emotions. Although emotions are very important for Scorpio, they manifest them differently than other water signs. In any case, you can be sure that the Scorpio will keep your secrets, whatever they may be."
+}, {
+	name: "Sagittarius",
+	img: "img/sagittarius.png",
+	detail: "November 22 - December 21 <br><u>Strengths</u>: Generous, idealistic, and a great sense of humor. <br><u>Weaknesses</u>: Promises more than can deliver, very impatient, and will say anything no matter how undiplomatic. <br><u>Description</u>: Curious and energetic, Sagittarius is one of the biggest travelers among all zodiac signs. Their open mind and philosophical view motivates them to wander around the world in search of the meaning of life. Sagittarius is extrovert, optimistic and enthusiastic, and likes changes. Sagittarius-born are able to transform their thoughts into concrete actions and they will do anything to achieve their goals."
+}, {
+	name: "Capricorn",
+	img: "img/capricorn.png",
+	detail: "December 22 - January 19 <br><u>Strengths</u>: Responsible, disciplined, self-control, and good managers. <br><u>Weaknesses</u>: Know-it-all, unforgiving, condescending, and expecting the worst. <br><u>Description</u>: Capricorn is a sign that represents time and responsibility, and its representatives are traditional and often very serious by nature. These individuals possess an inner state of independence that enables significant progress both in their personal and professional lives. They are masters of self-control and have the ability to lead the way, make solid and realistic plans, and manage many people who work for them at any time. They will learn from their mistakes and get to the top based solely on their experience and expertise."
+}, {
+	name: "Aquarius",
+	img: "img/aquarius.png",
+	detail: "January 20 - February 18 <br><u>Strengths</u>: Progressive, original, independent, and a humanitarian. <br><u>Weaknesses</u>: Runs from emotional expression, temperamental, uncompromising, and aloof. <br><u>Description</u>: Aquarius-born are shy and quiet, but on the other hand they can be eccentric and energetic. However, in both cases, they are deep thinkers and highly intellectual people who love helping others. They are able to see without prejudice, on both sides, which makes them people who can easily solve problems. Although they can easily adapt to the energy that surrounds them, Aquarius-born have a deep need to be some time alone and away from everything, in order to restore power. People born under the Aquarius sign, look at the world as a place full of possibilities."
+}, {
+	name: "Pisces",
+	img: "img/pisces.png",
+	detail: "February 19 - March 20 <br><u>Strengths</u>: Compassionate, artistic, intuitive, gentle, wise, and musical. <br><u>Weaknesses</u>: Fearful, overly trusting, sad, desire to escape reality, can be a victim or a martyr. <br><u>Description</u>: Pisces are very friendly, so they often find themselves in a company of very different people. Pisces are selfless, they are always willing to help others, without hoping to get anything back. Pisces is a Water sign and as such this zodiac sign is characterized by empathy and expressed emotional capacity."
+}]
+
+function getInfo() {
+	var sign = 
+	document.getElementById("sign").value.toLowerCase()
+
+	for(i = 0; i < horoscopes.length; i++) {
+		if (sign ==
+		horoscopes[i].name.toLowerCase()) {
+			document.getElementById("picture").src = horoscopes[i].img
+			document.getElementById("name").innerHTML = horoscopes[i].name
+			document.getElementById("detail").innerHTML = horoscopes[i].detail
+		}
+	}	
+}
